ff25f6be1bf4933f997219f802a0e706246545fd
JakubMycek <kuba.mycek.2008@gmail.com>
2023-05-11 10:14:30 +0200

    .

--- wisielec.py
+++ wisielec.py
@@ -1,26 +1,36 @@
 
 import random
 i=0
-wyrazy=["ala","kot","auto","pies"]
+wyrazy=["mysz","kot","auto","pies","mama","tata","dom","oko","wzrok","telewizor","rower","statek","samolot","deskorolka","rolki","hulajnoga","babcia","dziadek","cocia","wujek","kuzyn","kuzynka","brat","siostra","nos","usta","policzek","brew","włosy","czoło","chomik","papuga","słoń","ryba","rekin","blok","namiot","igloo","telefon","pilot","słuchawki","laptop","konsola","klawiatura","mysz"]
 wyraz = random.choice(wyrazy)
 listawyrazu=list(wyraz)
 długość=len(listawyrazu)
 wyrazgracz=[]
 długość2=len(wyrazgracz)
+listaliter=[]
 while długość > długość2:
     wyrazgracz.append("_")
     długość2=len(wyrazgracz)
 while i <= 10:
-    print(listawyrazu)
+    print(listaliter)
     print(wyrazgracz)
     litera=str(input("podaj literę"))
+    if litera in listaliter:
+        None
+    else:
+        listaliter.append(litera)
+    j=0
+    c=1
     for j in range(długość):
-        c=0
-        if litera==listawyrazu[j] and litera!=wyrazgracz[j]:
+        if litera == listawyrazu[j]:
             wyrazgracz[j]=litera
-            c=c+1
+            c=0
     if listawyrazu==wyrazgracz:
+        print(listawyrazu)
         print("wygrana")
+        break
+    if c!=0:
+            i=i+1
     if i==1:
         print("  ")
         print(" |")
@@ -91,9 +101,8 @@ while i <= 10:
         print(" |    /|\ ")
         print(" |    / \ ")
         print(" | ")
+        print(listawyrazu)
         print("przegrana")
-    if c==0:
-            i=i+1
 '''
     print("  ______")
     print(" | /   |")
